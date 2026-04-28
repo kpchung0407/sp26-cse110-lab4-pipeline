7cf55e969cb8bd02eee7afcd33e2c5c91080fccb
kpchung0407 <uyeong47@gmail.com>
2026-04-28 12:06:13 -0700

    fixed non number

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,6 +6,8 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
+    if(!(num1 == 'number'&&num2 == 'number'))
+        return false;
     if (add) {
         let result = 0;
 
